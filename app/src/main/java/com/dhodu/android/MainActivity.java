33e8055c8a0acbc81493b2cd7c3732cd74fa3554
Arnav Gupta <championswimmer@gmail.com>
2015-08-25 05:13:16 +0530

    create left pane
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- app/src/main/java/com/dhodu/android/MainActivity.java
+++ app/src/main/java/com/dhodu/android/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dhodu.android;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,16 +13,31 @@ import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
+    NavigationView leftNavView, rightNavView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ParseUser parseUser = ParseUser.getCurrentUser();
-        if ((parseUser == null)) {
-            Intent i = new Intent(this, LoginActivity.class);
-            startActivity(i);
-            finish();
+//        ParseUser parseUser = ParseUser.getCurrentUser();
+//        if ((parseUser == null)) {
+//            Intent i = new Intent(this, LoginActivity.class);
+//            startActivity(i);
+//            finish();
+//        }
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        leftNavView = (NavigationView) findViewById(R.id.nav_drawer_left);
+        rightNavView = (NavigationView) findViewById(R.id.nav_drawer_right);
+
+        drawerLayout.setBackgroundResource(R.drawable.app_background_doodle);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.dhodu_primary_dark));
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.dhodu_primary_dark));
         }
+
     }
 
     @Override
