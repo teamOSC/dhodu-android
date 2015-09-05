cf24eac660f9f286c498220d12a76d38b06e4172
naman14 <namandwivedi14@gmail.com>
2015-09-06 01:10:41 +0530

    layouts for different status

--- app/src/main/java/com/dhodu/android/CurrentOrderAdapter.java
+++ app/src/main/java/com/dhodu/android/CurrentOrderAdapter.java
@@ -6,25 +6,51 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.parse.ParseObject;
+
+import java.util.List;
+
 /**
  * Created by naman on 06/09/15.
  */
 public class CurrentOrderAdapter extends RecyclerView.Adapter<CurrentOrderAdapter.ItemHolder> {
 
     private Context context;
+    private List<ParseObject> arraylist;
 
-    public CurrentOrderAdapter(Context context) {
+    public CurrentOrderAdapter(Context context, List<ParseObject> arraylist) {
         this.context = context;
+        this.arraylist = arraylist;
     }
 
     @Override
     public CurrentOrderAdapter.ItemHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (viewType == 0) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_addnew_address, parent,false);
-            return new ItemHolder(v);
-        } else {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_address, parent,false);
-            return new ItemHolder(v);
+        View v;
+        switch (viewType) {
+            case 0:
+                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_current_order_0, parent, false);
+                return new ItemHolder(v);
+            case 1:
+                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_current_order_1, parent, false);
+                return new ItemHolder(v);
+            case 2:
+                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_current_order_2, parent, false);
+                return new ItemHolder(v);
+            case 3:
+                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_current_order_3, parent, false);
+                return new ItemHolder(v);
+            case 4:
+                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_current_order_4, parent, false);
+                return new ItemHolder(v);
+            case 5:
+                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_current_order_5, parent, false);
+                return new ItemHolder(v);
+            case 6:
+                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_current_order_6, parent, false);
+                return new ItemHolder(v);
+            default:
+                v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_current_order_6, parent, false);
+                return new ItemHolder(v);
         }
     }
 
@@ -35,7 +61,7 @@ public class CurrentOrderAdapter extends RecyclerView.Adapter<CurrentOrderAdapte
 
     @Override
     public int getItemCount() {
-       return 0;
+        return arraylist.size();
     }
 
     public class ItemHolder extends RecyclerView.ViewHolder {
@@ -49,9 +75,24 @@ public class CurrentOrderAdapter extends RecyclerView.Adapter<CurrentOrderAdapte
 
     @Override
     public int getItemViewType(int position) {
-        if (position == 0)
-            return 0;
-        else return 1;
+        switch (arraylist.get(position).getInt("status")) {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            case 3:
+                return 3;
+            case 4:
+                return 4;
+            case 5:
+                return 5;
+            case 6:
+                return 6;
+            default:
+                return 6;
+        }
     }
 
 }
--- app/src/main/java/com/dhodu/android/CurrentOrderFragment.java
+++ app/src/main/java/com/dhodu/android/CurrentOrderFragment.java
@@ -7,6 +7,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.dhodu.android.ui.DividerItemDecoration;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
 /**
  * Created by naman on 06/09/15.
  */
@@ -22,8 +31,26 @@ public class CurrentOrderFragment extends Fragment {
         final View view= inflater.inflate(R.layout.fragment_current_order, container, false);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.list_current_order);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),DividerItemDecoration.VERTICAL_LIST));
 
+        fetchCurrentOrders();
         return view;
+    }
+
+    private void fetchCurrentOrders() {
+        ParseQuery<ParseObject> query = new ParseQuery<>("Transaction");
+        query.whereEqualTo("customer", ParseUser.getCurrentUser());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+                if (e == null) {
+                    adapter = new CurrentOrderAdapter(getActivity(),list);
+                    recyclerView.setAdapter(adapter);
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
 }
