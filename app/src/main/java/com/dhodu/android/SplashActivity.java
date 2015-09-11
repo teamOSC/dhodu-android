181dc6a6c0ad61f63719370b9fa250825599ab73
Prempal Singh <prempal.42@gmail.com>
2015-09-11 18:53:36 +0530

    changes in splash screen as per requirements

--- app/src/main/java/com/dhodu/android/SplashActivity.java
+++ app/src/main/java/com/dhodu/android/SplashActivity.java
@@ -3,8 +3,10 @@ package com.dhodu.android;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.graphics.Typeface;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.View;
@@ -14,13 +16,13 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    LinearLayout imageSplash;
-    int width, height;
+    ImageView imageSplash;
 
     public void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -33,37 +35,47 @@ public class SplashActivity extends AppCompatActivity {
             getWindow().setNavigationBarColor(getResources().getColor(R.color.dhodu_primary_dark));
         }
 
-        Display display = getWindowManager().getDefaultDisplay();
-        width = display.getWidth();
-        height = display.getHeight();
-        imageSplash = (LinearLayout) findViewById(R.id.splash);
-        imageSplash.setVisibility(View.INVISIBLE);
-        TextView tagline = (TextView) findViewById(R.id.tagline);
-        TextView header = (TextView) findViewById(R.id.heading);
-        header.setTypeface(Typeface.createFromAsset(getAssets(), "Raleway-ExtraBold.ttf"));
+        imageSplash = (ImageView) findViewById(R.id.loader_image);
+        final TextView tagline = (TextView) findViewById(R.id.tagline);
         tagline.setTypeface(Typeface.createFromAsset(getAssets(), "Raleway-Regular.ttf"));
-        AnimationSet as = new AnimationSet(true);
-        Animation aa = new ScaleAnimation((float) 0.6
-                , (float) 0.5, (float) 0.6, (float) 0.5
-                , width / 2
-                , height / 2);
-        aa.setDuration(2000);
-        aa.setStartOffset(1000);
-        as.addAnimation(aa);
-        aa = new AlphaAnimation(0, 1);
-        aa.setDuration(2000);
-        aa.setStartOffset(1000);
-        as.addAnimation(aa);
-        as.setFillEnabled(true);
-        as.setFillAfter(true);
-        as.setInterpolator(new AccelerateInterpolator());
-        as.setStartTime(1000);
-        imageSplash.startAnimation(as);
-        imageSplash.setVisibility(View.VISIBLE);
+
+        final LinearLayout dhoduLayout = (LinearLayout) findViewById(R.id.splash);
+
+        AnimationDrawable ad = (AnimationDrawable) ContextCompat.getDrawable(this,
+                R.drawable.loader_icon);
+        imageSplash.setImageDrawable(ad);
+        ad.start();
+
         Runnable runnable = new Runnable() {
             public void run() {
                 try {
-                    Thread.sleep(4000);
+                    Thread.sleep(3000);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Display display = getWindowManager().getDefaultDisplay();
+                            imageSplash.setVisibility(View.GONE);
+                            AnimationSet as = new AnimationSet(true);
+                            Animation aa = new ScaleAnimation((float) 0.6
+                                    , (float) 0.5, (float) 0.6, (float) 0.5
+                                    , display.getWidth() / 2
+                                    , display.getHeight() / 2);
+                            aa.setDuration(2000);
+                            aa.setStartOffset(1000);
+                            as.addAnimation(aa);
+                            aa = new AlphaAnimation(0, 1);
+                            aa.setDuration(2000);
+                            aa.setStartOffset(0);
+                            as.addAnimation(aa);
+                            as.setFillEnabled(true);
+                            as.setFillAfter(true);
+                            as.setInterpolator(new AccelerateInterpolator());
+                            as.setStartTime(1000);
+                            dhoduLayout.startAnimation(as);
+                            dhoduLayout.setVisibility(View.VISIBLE);
+                        }
+                    });
+                    Thread.sleep(3000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
