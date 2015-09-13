ec61f30f636421d60b751903d58057fa805500c9
Prempal Singh <prempal.42@gmail.com>
2015-09-14 02:56:11 +0530

    basic parse push setup

--- app/src/main/java/com/dhodu/android/App.java
+++ app/src/main/java/com/dhodu/android/App.java
@@ -3,6 +3,7 @@ package com.dhodu.android;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 
 /**
  * Created by prempal on 25/8/15.
@@ -13,5 +14,6 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         Parse.initialize(this, "8hl3wDwaozzqWEGdkN8aCaLdWOLNnAxPlXXj53d0", "ado3ZoaVnQgpTixUIWiomvwzYcoPqAijSMWGISMM");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 }
