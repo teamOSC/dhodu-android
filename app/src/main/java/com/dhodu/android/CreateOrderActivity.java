bc8b203ddbc0befcec6f5ba485b7498ec2583a38
naman14 <namandwivedi14@gmail.com>
2015-09-16 01:35:31 +0530

    place order from new activity

--- app/src/main/java/com/dhodu/android/CreateOrderActivity.java
+++ app/src/main/java/com/dhodu/android/CreateOrderActivity.java
@@ -1,23 +1,246 @@
 package com.dhodu.android;
 
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.dhodu.android.addresses.MyAddressesActivity;
+import com.google.android.gms.maps.model.LatLng;
+import com.parse.CountCallback;
+import com.parse.GetCallback;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 /**
  * Created by naman on 16/09/15.
  */
 public class CreateOrderActivity extends AppCompatActivity {
 
+    View submitOrder;
+    TextView locationAddress;
+    TimePicker timePicker;
+    RadioButton today, tomorrow, datomorrow;
+    CheckBox press;
+    CheckBox washPress;
+    CheckBox dryclean;
+    Switch expressSwitch;
+
+    LatLng latLng;
+    int locationShifted = 0;
+
+    int addressindex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_order);
 
-        Toolbar toolbar =(Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        submitOrder = findViewById(R.id.submit_order);
+        locationAddress = (TextView) findViewById(R.id.location_address);
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        today = (RadioButton) findViewById(R.id.radio_today);
+        tomorrow = (RadioButton) findViewById(R.id.radio_tomorrow);
+        datomorrow = (RadioButton) findViewById(R.id.radio_datomorrow);
+        press = (CheckBox) findViewById(R.id.cb_press);
+        washPress = (CheckBox) findViewById(R.id.cb_wash_press);
+        dryclean = (CheckBox) findViewById(R.id.cb_dryclean);
+        expressSwitch = (Switch) findViewById(R.id.express_switch);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("New order");
+
+        setCurrentLocation(this);
+
+        expressSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    Toast.makeText(getBaseContext(), "Express service coming soon", Toast.LENGTH_SHORT).show();
+                    expressSwitch.setChecked(false);
+                }
+            }
+        });
+
+        locationAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateOrderActivity.this, MyAddressesActivity.class);
+                intent.setAction("chooseAddress");
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        submitOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Transaction");
+                query.countInBackground(new CountCallback() {
+                    @Override
+                    public void done(int count, com.parse.ParseException e) {
+                        if (e != null) {
+                            Toast.makeText(getBaseContext(), "Oops! Something went wrong", Toast.LENGTH_SHORT).show();
+                        } else {
+                            String serviceTypes = "";
+                            if (press.isChecked())
+                                serviceTypes = serviceTypes + 0;
+                            if (washPress.isChecked())
+                                serviceTypes = serviceTypes + 1;
+                            if (dryclean.isChecked())
+                                serviceTypes = serviceTypes + 2;
+                            final ParseObject transaction = new ParseObject("Transaction");
+                            transaction.put("status", 0);
+                            transaction.put("transaction_id", count + 1);
+                            transaction.put("customer", ParseUser.getCurrentUser());
+                            transaction.put("time_pick", "");
+                            transaction.put("pick_date", "12-09-2015");
+                            transaction.put("address_index", addressindex);
+                            transaction.put("comments", "");
+                            ParseGeoPoint geoPoint = new ParseGeoPoint(latLng.latitude, latLng.longitude);
+                            transaction.put("location", geoPoint);
+                            transaction.put("service_type", serviceTypes);
+                            ParseQuery<ParseObject> query = ParseQuery.getQuery("Manager");
+                            query.whereNear("location", geoPoint);
+                            query.getFirstInBackground(new GetCallback<ParseObject>() {
+                                @Override
+                                public void done(ParseObject object, com.parse.ParseException e) {
+                                    if (e == null) {
+                                        transaction.put("assigned_manager", object);
+                                        transaction.saveInBackground(new SaveCallback() {
+                                            @Override
+                                            public void done(com.parse.ParseException e) {
+                                                if (e == null) {
+                                                    Toast.makeText(getBaseContext(), "Order placed successfully!", Toast.LENGTH_SHORT).show();
+                                                } else {
+                                                    Toast.makeText(getBaseContext(), "Oops! Something went wrong", Toast.LENGTH_SHORT).show();
+                                                }
+                                            }
+                                        });
+                                    } else {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            });
+
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                addressindex = data.getIntExtra("address_index", 0);
+                String name = data.getStringExtra("address_name");
+                locationAddress.setText(name);
+            }
+            if (resultCode == RESULT_CANCELED) {
+            }
+
+        }
+
+    }
+
+    public void setCurrentLocation(Context context) {
+
+        latLng = new LatLng(0, 0);
+        final LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+
+        try {
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER, 1000, 0,
+                    new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            locationShifted++;
+                            if (locationShifted > 2) {
+                                try {
+                                    locationManager.removeUpdates(this);
+                                } catch (SecurityException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    });
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
+        try {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000, 0, new LocationListener() {
+                        @Override
+                        public void onLocationChanged(Location location) {
+                            latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            locationShifted++;
+                            if (locationShifted > 2) {
+                                try {
+                                    locationManager.removeUpdates(this);
+                                } catch (SecurityException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                        }
+
+                        @Override
+                        public void onProviderEnabled(String provider) {
+
+                        }
+
+                        @Override
+                        public void onProviderDisabled(String provider) {
+
+                        }
+                    });
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- app/src/main/java/com/dhodu/android/MainActivity.java
+++ app/src/main/java/com/dhodu/android/MainActivity.java
@@ -2,15 +2,10 @@ package com.dhodu.android;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.hardware.Camera;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -26,33 +21,23 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import com.dhodu.android.addresses.MyAddressesActivity;
 import com.dhodu.android.login.LoginActivity;
 import com.dhodu.android.ui.CircleImageView;
-import com.google.android.gms.maps.model.LatLng;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
-import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.squareup.picasso.Picasso;
 
 import java.io.BufferedInputStream;
@@ -68,22 +53,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
     DrawerLayout drawerLayout;
-    SlidingUpPanelLayout slideUpLayout;
-    SurfaceView surfaceView;
-    SurfaceHolder surfaceHolder;
-    Camera camera;
-    FrameLayout cameraContainer;
     LinearLayout topView;
-    float pullDownRatio;
-    boolean cameraShowing;
-
-    EditText orderTime;
-    Button submitOrder;
-    Switch expressSwitch;
-    TextView locationAddress;
-    CheckBox press;
-    CheckBox washPress;
-    CheckBox dryclean;
 
     TextView tvProfileMobile;
     TextView profileName;
@@ -91,13 +61,9 @@ public class MainActivity extends AppCompatActivity {
     ImageView editProfile;
     Toolbar toolbar;
 
-    int addressindex = 0;
-    int locationShifted = 0;
-    RecyclerView centerRecyclerview;
-    ProgressBar progressBar;
+
     View statusView;
     private String photoPath;
-    private LatLng latLng;
     private TextView orderStatus;
     private ImageView expandCreateOrder;
 
@@ -113,14 +79,10 @@ public class MainActivity extends AppCompatActivity {
         } else {
             // Instantiate the layouts
             drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-            cameraContainer = (FrameLayout) findViewById(R.id.camera_container);
             topView = (LinearLayout) findViewById(R.id.topView);
             toolbar = (Toolbar) findViewById(R.id.toolbar);
-            locationAddress = (TextView) findViewById(R.id.location_address);
             orderStatus = (TextView) findViewById(R.id.orderStatus);
             expandCreateOrder = (ImageView) findViewById(R.id.expand);
-            centerRecyclerview = (RecyclerView) findViewById(R.id.center_recyclerview);
-            progressBar = (ProgressBar) findViewById(R.id.progressBar);
             editProfile = (ImageView) findViewById(R.id.edit_profile);
             statusView = findViewById(R.id.statusView);
 
@@ -181,9 +143,6 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
 
-//            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//            transaction.replace(R.id.current_fragment_container, new CurrentOrderFragment());
-//            transaction.commit();
 
             //Setup the order pulldown screen
 //            orderTime = (EditText) findViewById(R.id.order_time);
@@ -215,191 +174,28 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            });
 //
-//            locationAddress.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent intent = new Intent(MainActivity.this, MyAddressesActivity.class);
-//                    intent.setAction("chooseAddress");
-//                    startActivityForResult(intent, 0);
-//                }
-//            });
-//
-//            expressSwitch = (Switch) findViewById(R.id.express_switch);
-//            expressSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//                @Override
-//                public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-//                    if (b) {
-//                        Toast.makeText(getBaseContext(), "Express service coming soon", Toast.LENGTH_SHORT).show();
-//                        expressSwitch.setChecked(false);
-//                    }
-//                }
-//            });
-//
-//            press = (CheckBox) findViewById(R.id.cb_press);
-//            washPress = (CheckBox) findViewById(R.id.cb_wash_press);
-//            dryclean = (CheckBox) findViewById(R.id.cb_dryclean);
-//
-//            submitOrder = (Button) findViewById(R.id.btn_submit_order);
-//            submitOrder.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    ParseQuery<ParseObject> query = ParseQuery.getQuery("Transaction");
-//                    query.countInBackground(new CountCallback() {
-//                        @Override
-//                        public void done(int count, ParseException e) {
-//                            if (e != null) {
-//                                Toast.makeText(getBaseContext(), "Oops! Something went wrong", Toast.LENGTH_SHORT).show();
-//                            } else {
-//                                String serviceTypes = "";
-//                                if (press.isChecked())
-//                                    serviceTypes = serviceTypes + 0;
-//                                if (washPress.isChecked())
-//                                    serviceTypes = serviceTypes + 1;
-//                                if (dryclean.isChecked())
-//                                    serviceTypes = serviceTypes + 2;
-//                                final ParseObject transaction = new ParseObject("Transaction");
-//                                transaction.put("status", 0);
-//                                transaction.put("transaction_id", count + 1);
-//                                transaction.put("customer", ParseUser.getCurrentUser());
-//                                transaction.put("time_pick", orderTime.getText().toString());
-//                                transaction.put("pick_date", "12-09-2015");
-//                                transaction.put("address_index", addressindex);
-//                                transaction.put("comments", "");
-//                                ParseGeoPoint geoPoint = new ParseGeoPoint(latLng.latitude, latLng.longitude);
-//                                transaction.put("location", geoPoint);
-//                                transaction.put("service_type", serviceTypes);
-//                                ParseQuery<ParseObject> query = ParseQuery.getQuery("Manager");
-//                                query.whereNear("location", geoPoint);
-//                                query.getFirstInBackground(new GetCallback<ParseObject>() {
-//                                    @Override
-//                                    public void done(ParseObject object, ParseException e) {
-//                                        if (e == null) {
-//                                            transaction.put("assigned_manager", object);
-//                                            transaction.saveInBackground(new SaveCallback() {
-//                                                @Override
-//                                                public void done(ParseException e) {
-//                                                    if (e == null) {
-//                                                        Toast.makeText(getBaseContext(), "Order placed successfully!", Toast.LENGTH_SHORT).show();
-//                                                    } else {
-//                                                        Toast.makeText(getBaseContext(), "Oops! Something went wrong", Toast.LENGTH_SHORT).show();
-//                                                    }
-//                                                }
-//                                            });
-//                                        } else {
-//                                            e.printStackTrace();
-//                                        }
-//
-//                                    }
-//                                });
-//
-//                            }
-//                        }
-//                    });
-//                }
-//            });
+
 
             editProfile.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    startActivity(new Intent(MainActivity.this,ProfileActivity.class));
+                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
                 }
             });
 
-//            surfaceView = (SurfaceView) findViewById(R.id.surface_view);
-//            surfaceHolder = surfaceView.getHolder();
-//            surfaceHolder.addCallback(new SurfaceHolder.Callback() {
-//                @Override
-//                public void surfaceCreated(SurfaceHolder surfaceHolder) {
-//
-//                }
-//
-//                @Override
-//                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-//
-//                }
-//
-//                @Override
-//                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-//
-//                }
-//            });
-
-//            cameraContainer.setVisibility(View.GONE);
-//
-//            slideUpLayout.setPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
-//                @Override
-//                public void onPanelSlide(View view, float v) {
-//                    cameraContainer.setVisibility(View.VISIBLE);
-//                    cameraContainer.setAlpha(v);
-//                    topView.setAlpha(1 - v);
-//                }
-//
-//                @Override
-//                public void onPanelCollapsed(View view) {
-//                    topView.setVisibility(View.VISIBLE);
-//                    //Stop the camera view here
-//                    try {
-//                        camera.stopPreview();
-//                        camera.release();
-//                        cameraShowing = false;
-//                    } catch (Exception e) {
-//                        Log.e(TAG, "onPanelCollapsed ", e);
-//                    }
-//                }
-//
-//                @Override
-//                public void onPanelExpanded(View view) {
-//                    //Initialise the camera view here
-//                    topView.setVisibility(View.GONE);
-//                    try {
-//                        camera = Camera.open();
-//                        Camera.Parameters params = camera.getParameters();
-//                        camera.setDisplayOrientation(90);
-//                        camera.setParameters(params);
-//
-//                        camera.setPreviewDisplay(surfaceHolder);
-//                        cameraShowing = true;
-//                    } catch (Exception e) {
-//                        Log.e(TAG, "onPanelExpanded ", e);
-//                    }
-//                    camera.startPreview();
-//
-//                }
-//
-//                @Override
-//                public void onPanelAnchored(View view) {
-//
-//                }
-//
-//                @Override
-//                public void onPanelHidden(View view) {
-//
-//                }
-//            });
-
-            //drawerLayout.setBackgroundResource(R.drawable.app_background_doodle);
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 getWindow().setStatusBarColor(getResources().getColor(R.color.dhodu_primary_dark));
                 getWindow().setNavigationBarColor(getResources().getColor(R.color.dhodu_primary_dark));
             }
 
-            setCurrentLocation(this);
         }
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-                addressindex = data.getIntExtra("address_index", 0);
-                String name = data.getStringExtra("address_name");
-                locationAddress.setText(name);
-            }
-            if (resultCode == RESULT_CANCELED) {
-            }
-        } else if (requestCode == 69) {
+        if (requestCode == 69) {
             if (resultCode == RESULT_CANCELED) {
                 File photoFile = new File(photoPath);
                 if (photoFile.exists())
@@ -451,10 +247,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        if (slideUpLayout.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED) {
-            slideUpLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
-            return;
-        }
         super.onBackPressed();
     }
 
@@ -568,86 +360,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-    public void setCurrentLocation(Context context) {
-
-        latLng = new LatLng(0, 0);
-        final LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-
-        try {
-            locationManager.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER, 1000, 0,
-                    new LocationListener() {
-                        @Override
-                        public void onLocationChanged(Location location) {
-                            latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                            locationShifted++;
-                            if (locationShifted > 2) {
-                                try {
-                                    locationManager.removeUpdates(this);
-                                } catch (SecurityException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                        }
-
-                        @Override
-                        public void onProviderEnabled(String provider) {
-
-                        }
-
-                        @Override
-                        public void onProviderDisabled(String provider) {
-
-                        }
-                    });
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-
-        try {
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                    1000, 0, new LocationListener() {
-                        @Override
-                        public void onLocationChanged(Location location) {
-                            latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                            locationShifted++;
-                            if (locationShifted > 2) {
-                                try {
-                                    locationManager.removeUpdates(this);
-                                } catch (SecurityException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-
-                        @Override
-                        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                        }
-
-                        @Override
-                        public void onProviderEnabled(String provider) {
-
-                        }
-
-                        @Override
-                        public void onProviderDisabled(String provider) {
-
-                        }
-                    });
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-
-    }
-
     public void setStatusToHeader(String status, int imageId) {
         statusView.setVisibility(View.VISIBLE);
         if (imageId != 0) {
@@ -680,8 +392,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         Adapter adapter = new Adapter(getSupportFragmentManager());
-        adapter.addFragment(new CurrentOrderFragment(),"Order");
-        adapter.addFragment(new OrderHistoryFragment(),"History");
+        adapter.addFragment(new CurrentOrderFragment(), "Order");
+        adapter.addFragment(new OrderHistoryFragment(), "History");
         viewPager.setAdapter(adapter);
     }
 
