8413ecff007925b28d3183edc77a55a4374afd7d
Prempal Singh <prempal.42@gmail.com>
2015-11-02 05:28:50 +0530

    skip splash screen if on DEBUG

--- app/src/main/java/com/dhodu/android/SplashActivity.java
+++ app/src/main/java/com/dhodu/android/SplashActivity.java
@@ -29,60 +29,66 @@ public class SplashActivity extends AppCompatActivity {
         getWindow().setFormat(PixelFormat.RGBA_8888);
         setContentView(R.layout.activity_splash);
 
-        imageSplash = (ImageView) findViewById(R.id.loader_image);
-        final TextView tagline = (TextView) findViewById(R.id.tagline);
-        tagline.setTypeface(Typeface.createFromAsset(getAssets(), "Raleway-Regular.ttf"));
+        if (BuildConfig.DEBUG) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        } else {
 
-        final LinearLayout dhoduLayout = (LinearLayout) findViewById(R.id.splash);
+            imageSplash = (ImageView) findViewById(R.id.loader_image);
+            final TextView tagline = (TextView) findViewById(R.id.tagline);
+            tagline.setTypeface(Typeface.createFromAsset(getAssets(), "Raleway-Regular.ttf"));
 
-        AnimationDrawable ad = (AnimationDrawable) ContextCompat.getDrawable(this,
-                R.drawable.loader_icon);
-        imageSplash.setImageDrawable(ad);
-        ad.start();
+            final LinearLayout dhoduLayout = (LinearLayout) findViewById(R.id.splash);
 
-        Runnable runnable = new Runnable() {
-            public void run() {
-                try {
-                    Thread.sleep(3000);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Display display = getWindowManager().getDefaultDisplay();
-                            imageSplash.setVisibility(View.GONE);
-                            AnimationSet as = new AnimationSet(true);
-                            Animation aa = new ScaleAnimation((float) 0.6
-                                    , (float) 0.5, (float) 0.6, (float) 0.5
-                                    , display.getWidth() / 2
-                                    , display.getHeight() / 2);
-                            aa.setDuration(2000);
-                            aa.setStartOffset(1000);
-                            as.addAnimation(aa);
-                            aa = new AlphaAnimation(0, 1);
-                            aa.setDuration(2000);
-                            aa.setStartOffset(0);
-                            as.addAnimation(aa);
-                            as.setFillEnabled(true);
-                            as.setFillAfter(true);
-                            as.setInterpolator(new AccelerateInterpolator());
-                            as.setStartTime(1000);
-                            dhoduLayout.startAnimation(as);
-                            dhoduLayout.setVisibility(View.VISIBLE);
-                        }
-                    });
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            AnimationDrawable ad = (AnimationDrawable) ContextCompat.getDrawable(this,
+                    R.drawable.loader_icon);
+            imageSplash.setImageDrawable(ad);
+            ad.start();
+
+            Runnable runnable = new Runnable() {
+                public void run() {
+                    try {
+                        Thread.sleep(3000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Display display = getWindowManager().getDefaultDisplay();
+                                imageSplash.setVisibility(View.GONE);
+                                AnimationSet as = new AnimationSet(true);
+                                Animation aa = new ScaleAnimation((float) 0.6
+                                        , (float) 0.5, (float) 0.6, (float) 0.5
+                                        , display.getWidth() / 2
+                                        , display.getHeight() / 2);
+                                aa.setDuration(2000);
+                                aa.setStartOffset(1000);
+                                as.addAnimation(aa);
+                                aa = new AlphaAnimation(0, 1);
+                                aa.setDuration(2000);
+                                aa.setStartOffset(0);
+                                as.addAnimation(aa);
+                                as.setFillEnabled(true);
+                                as.setFillAfter(true);
+                                as.setInterpolator(new AccelerateInterpolator());
+                                as.setStartTime(1000);
+                                dhoduLayout.startAnimation(as);
+                                dhoduLayout.setVisibility(View.VISIBLE);
+                            }
+                        });
+                        Thread.sleep(3000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(SplashActivity.this);
+                    Intent intent;
+                    if (sp.getBoolean("app_first_run", true))
+                        intent = new Intent(SplashActivity.this, DhoduIntro.class);
+                    else
+                        intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
                 }
-                SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(SplashActivity.this);
-                Intent intent;
-                if (sp.getBoolean("app_first_run", true))
-                    intent = new Intent(SplashActivity.this, DhoduIntro.class);
-                else
-                    intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                SplashActivity.this.finish();
-            }
-        };
-        new Thread(runnable).start();
+            };
+            new Thread(runnable).start();
+        }
     }
 }
