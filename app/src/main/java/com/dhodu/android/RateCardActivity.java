e142142d579f6b9d176246ffa13611567d6656e8
Prempal Singh <prempal.42@gmail.com>
2015-11-04 00:25:55 +0530

    rate card view pager

--- app/src/main/java/com/dhodu/android/RateCardActivity.java
+++ app/src/main/java/com/dhodu/android/RateCardActivity.java
@@ -1,24 +1,16 @@
 package com.dhodu.android;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.parse.FindCallback;
-import com.parse.ParseException;
 import com.parse.ParseObject;
-import com.parse.ParseQuery;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class RateCardActivity extends AppCompatActivity {
@@ -39,75 +31,18 @@ public class RateCardActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if (viewPager != null) {
+            Adapter adapter = new Adapter(getSupportFragmentManager());
+            adapter.addFragment(RateCardFragment.newInstance(RateCardFragment.MENS_WEAR), RateCardFragment.MENS_WEAR);
+            adapter.addFragment(RateCardFragment.newInstance(RateCardFragment.WOMEN_WEAR), RateCardFragment.WOMEN_WEAR);
+            adapter.addFragment(RateCardFragment.newInstance(RateCardFragment.HOUSEHOLD), RateCardFragment.HOUSEHOLD);
+            viewPager.setAdapter(adapter);
+            viewPager.setOffscreenPageLimit(2);
+        }
 
-        final Spinner serviceType = (Spinner) findViewById(R.id.service_type);
-        final Spinner category = (Spinner) findViewById(R.id.category);
-
-        final String[] categories = new String[]{"MEN'S WEAR", "WOMEN WEAR", "KIDS WEAR", "HOUSEHOLD", "OTHERS"};
-        final String[] serviceTypes = new String[]{"Wash & Press", "Drycleaning"};
-
-        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Arrays.asList(categories));
-        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        category.setAdapter(categoryAdapter);
-
-        ArrayAdapter<String> servicesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, Arrays.asList(serviceTypes));
-        servicesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        serviceType.setAdapter(servicesAdapter);
-
-        category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                List<ParseObject> list = new ArrayList<>();
-                if (clothesList != null) {
-                    for (ParseObject cloth : clothesList) {
-                        if (cloth.getString("category").equals(categories[i]))
-                            list.add(cloth);
-                    }
-                    recyclerView.setAdapter(new RateCardAdapter(list, serviceType.getSelectedItemPosition()));
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        serviceType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                List<ParseObject> list = new ArrayList<>();
-                if (clothesList != null) {
-                    for (ParseObject cloth : clothesList) {
-                        if (cloth.getString("category").equals(categories[category.getSelectedItemPosition()]))
-                            list.add(cloth);
-                    }
-                }
-                recyclerView.setAdapter(new RateCardAdapter(list, i));
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-        ParseQuery<ParseObject> query = new ParseQuery<>("Clothes");
-        query.findInBackground(new FindCallback<ParseObject>() {
-            @Override
-            public void done(List<ParseObject> objects, ParseException e) {
-                progressBar.setVisibility(View.GONE);
-                if (e == null) {
-                    clothesList = objects;
-                    recyclerView.setAdapter(new RateCardAdapter(clothesList, serviceType.getSelectedItemPosition()));
-                } else {
-                    e.printStackTrace();
-                    Toast.makeText(RateCardActivity.this, "Error fetching rate list", Toast.LENGTH_SHORT).show();
-                }
-
-            }
-        });
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 }
---
+++ app/src/main/java/com/dhodu/android/RateCardFragment.java
@@ -0,0 +1,87 @@
+package com.dhodu.android;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RateCardFragment extends Fragment {
+
+    public static final String MENS_WEAR = "MEN'S WEAR";
+    public static final String WOMEN_WEAR = "WOMEN WEAR";
+    public static final String HOUSEHOLD = "HOUSEHOLD";
+
+
+    public RateCardFragment() {
+        // Required empty public constructor
+    }
+
+    public static RateCardFragment newInstance(String category) {
+
+        Bundle args = new Bundle();
+        RateCardFragment fragment = new RateCardFragment();
+        args.putString("category", category);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_rate_card, container, false);
+        final ProgressBar progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
+
+        final Spinner serviceType = (Spinner) rootView.findViewById(R.id.service_type);
+        final String[] serviceTypes = new String[]{"Wash & Press", "Drycleaning"};
+
+        final RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        ArrayAdapter<String> servicesAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, Arrays.asList(serviceTypes));
+        servicesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        serviceType.setAdapter(servicesAdapter);
+
+        ParseQuery<ParseObject> query = new ParseQuery<>("Clothes");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                progressBar.setVisibility(View.GONE);
+                if (e == null) {
+                    List<ParseObject> list = new ArrayList<>();
+                    for (ParseObject cloth : objects) {
+                        if (cloth.getString("category").equals(getArguments().getString("category")))
+                            list.add(cloth);
+                    }
+                    recyclerView.setAdapter(new RateCardAdapter(list, serviceType.getSelectedItemPosition()));
+                } else {
+                    e.printStackTrace();
+                    Toast.makeText(getActivity(), "Error fetching rate list", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        return rootView;
+    }
+
+}
